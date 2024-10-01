c0b02a447080de8f6fb9cc01431279cdff7dc33c
AndjDavis <andi.idavis@gmail.com>
2024-10-01 13:43:12 -0600

    search bar toggles visibility when search icon is clicked

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const searchButton = document.getElementById("searchBtn");
+	const searchBar = document.querySelector(".searchBar");
+	const searchInput = document.getElementById("searchInput");
+	const searchClose = document.getElementById("searchClose");
+
+	searchButton.addEventListener("click", function () {
+		searchBar.style.visibility = "visible";
+		searchBar.classList.add("open");
+		this.setAttribute("aria-expanded", "true");
+		searchInput.focus();
+	});
+
+	searchClose.addEventListener("click", function () {
+		searchBar.style.visibility = "hidden";
+		searchBar.classList.remove("open");
+		this.setAttribute("aria-expanded", "false");
+	});
+});
