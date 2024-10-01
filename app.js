d8c3b3e731799d20591659a4376d10dc13039b73
AndjDavis <andi.idavis@gmail.com>
2024-10-01 12:13:05 -0600

    added middleware for searching

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 // connect to database
 connectDB();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
 // Templating Engine
