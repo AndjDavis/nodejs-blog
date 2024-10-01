4a6ab41d44b28bfc58e1bb4370c3add19dd68fc6
AndjDavis <andi.idavis@gmail.com>
2024-10-01 15:56:27 -0600

    added login and register routes and functionality.

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
 const connectDB = require("./server/config/db");
 const logger = require("./server/middleware");
 
-// create the express application
+// Create the express application
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// connect to database
+// Connect to database
 connectDB();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Add public directory
 app.use(express.static("public"));
 
 // Templating Engine
@@ -22,12 +25,27 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 // Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true,
+		store: MongoStore.create({
+			mongoUrl: process.env.MONGODB_URI,
+		}),
+		// cookie: { maxAge: new Date ( Date.now() + (3600000) ) }
+	})
+);
 app.use(logger);
 
-// use as homepage
+// Routes
 app.use("/", require("./server/routes/main"));
 app.use("/admin", require("./server/routes/admin"));
 
+// Default locals values
 app.locals.title = "NodeJS Blog";
 app.locals.description = "Simple blog created with NodeJS, Express, & MongoDB";
 
