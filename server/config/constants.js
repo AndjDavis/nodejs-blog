f4ec5fa25026dfeb5baa1eaa01d6c5a39325974c
AndjDavis <andi.idavis@gmail.com>
2024-10-03 11:12:02 -0600

    updated constants and route title map

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,13 +1,29 @@
 const adminLayout = "../views/layouts/admin";
 const mainLayout = "./layouts/main";
+const mainRouter = "main";
+const adminRouter = "admin";
+const postsRouter = "posts";
+
 const localsTitleMap = {
-	"/": "Log In",
-	"/register": "Register",
-	"/dashboard": "Dashboard",
-	"/add-post": "Add Post",
-	"/edit-post/:id": "Edit Post",
+	[mainRouter]: {
+		"/search": "Search Results",
+		"/about": "About",
+		"/contact": "Contact",
+	},
+	[adminRouter]: {
+		"/": "Log In",
+		"/dashboard": "Dashboard",
+		"/register": "Register",
+	},
+	[postsRouter]: {
+		"/add-post": "Add Post",
+		"/edit-post/:id": "Edit Post",
+	},
 };
 
 module.exports.adminLayout = adminLayout;
 module.exports.localsTitleMap = localsTitleMap;
 module.exports.mainLayout = mainLayout;
+module.exports.mainRouter = mainRouter;
+module.exports.adminRouter = adminRouter;
+module.exports.postsRouter = postsRouter;
